247936a16f529f3511b8c39e16c9918ffeb4189f
北歸 <112842987+Camel-Li@users.noreply.github.com>
2022-09-18 11:40:55 +0800

    9月18日（修改）
    9月18日，对代码进行了一部分优化更新，对各省疫情页面的图表做了修复（图例显示问题）。其次，echarts的动态图在主页会出现option内容兼并现象，目前无法处理掉港澳台饼图最开始不显示的问题，饼图需要鼠标移动到折线图上才会显示对应日期的信息，这个bug到现在还无法修复，没做到第一眼的视觉体验，正在努力学习中。
    其他的小细节就不多说了，大致更新就这样。可联系QQ：1719446064。

---
+++ 9月18日（修改）/main.py
@@ -0,0 +1,30 @@
+import os
+from corona_virus_spider import CoronaVirusSpider, CoronaVirusWebmake
+
+excel_name_1 = 'data_initial'  # 可设置第一个excel文件名，不带后缀
+excel_name_2 = "data_after_collation"  # 可设置第一个excel文件名，不带后缀
+
+
+# 将运行爬虫写如方法，方便开关
+def run_spider(begin, end):
+    spider = CoronaVirusSpider(excel_name_1)
+    sheet_days_in_data = spider.spider_excel_run(begin, end)
+    test = CoronaVirusWebmake(sheet_days_in_data, excel_name_1, excel_name_2)
+    test.excel_run()
+
+
+# 操作文件的主程序，爬虫每获取一页信息设置sleep(1)，主要是为了降低爬取速度保证安全
+if __name__ == '__main__':
+    begin_page = 1
+    end_page = 2
+    # 一下两个开关分别是--爬虫数据生成--和--web服务器搭建--，可以进行有选择的开关
+    run_spider(begin_page, end_page)
+    os.system("python my_app_flask.py")
+
+    # 手动打开网页：http://127.0.0.1:5000
+    # 自动打开暂时无法在同一个python里打开，会出很多问题。
+    # 运行过程中发现os.system会阻塞进程，尝试用信号控制kill子进程，但是又没有意外关闭机制，会生成僵尸进程，尝试了很多方法后，没有感觉比手动打开网页更方便
+    # 原本是使用subprocess.Popen，但是这会生成多线程，导致很多情况下无法即使kill到服务器，使得服务器大量占用内存
+    # 最终还是选择人工打开页面，或者另写一个打开页面的程序，尝试过这样子做，但是与作业无关，就没有放在这里了
+    # url_host = 'http://127.0.0.1:5000'
+    # webbrowser.open(url_host)
